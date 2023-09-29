a994550313aa0ee786ec491e71bfcf840510e13e
Emmanuelbinen <emmanuelbinen@outlook.com>
2023-09-29 17:10:00 +0200

    deleting unnecessary code from the sub branch

--- mainFile.js
+++ mainFile.js
@@ -1,29 +1,3 @@
-// Create an array of objects
-const messages = [
-    {
-      subject: "You",
-      adjectives: ["smart", "funny", "kind", "talented", "amazing"],
-      verbs: ["are", "can be", "will be", "should be", "must be"],
-      objects: ["do anything you set your mind to", "achieve great things", "make a difference in the world"],
-      adverbialPhrases: ["with ease", "with passion", "with dedication"],
-    },
-    {
-      subject: "The world",
-      adjectives: ["big", "beautiful", "complex", "fascinating", "mysterious"],
-      verbs: ["needs"],
-      objects: ["your intelligence", "your creativity", "your empathy", "your passion", "your dedication"],
-      adverbialPhrases: ["more than ever", "now more than ever"],
-    },
-    {
-      subject: "Happiness",
-      adjectives: ["true", "lasting", "genuine", "fulfilling", "deep"],
-      verbs: ["is found"],
-      objects: ["in helping others", "in pursuing your dreams", "in appreciating the simple things in life", "in being kind to yourself and others", "in finding your purpose"],
-      adverbialPhrases: ["when you least expect it"],
-    },
-  ];
-
-  
 // Generate a random index into the array
 const randomIndex = Math.floor(Math.random() * messages.length);
 
